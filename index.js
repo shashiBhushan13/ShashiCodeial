f4c32aeb5186c71631291913277d6f41780b0139
Shashi <shashibhushan8683@gmail.com>
2020-05-29 17:40:10 +0530

    Added Sign In and Sign Up

---
+++ index.js
@@ -0,0 +1,40 @@
+const express=require('express');
+const cookieParser=require('cookie-parser');
+const app=express();
+
+const port=8000;
+
+const expressLayouts=require('express-ejs-layouts');
+
+const db=require('./config/mongoose');
+app.use(express.urlencoded());
+
+app.use(cookieParser());
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+
+//extract style and scripts from sub pages into the layout 
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+//use express router
+
+app.use('/',require('./routes'));
+
+//setup the view engine
+
+app.set('view engine','ejs');
+
+app.set('views', './views');
+
+app.listen(port,function(err){
+
+if(err)
+{
+   
+    console.log(`Error in running the server: ${err}`);
+}
+
+console.log(`Server is running in the port: ${port}`);
+
+});
